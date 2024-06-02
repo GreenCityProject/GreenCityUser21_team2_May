42dce0d2fd4a44af19011a1c6d9850e406964b50
tellmant <petukhov.m05@gmail.com>
2024-06-02 23:30:07 +0300

    [106] add FriendDto to display info about user's friend

---
+++ service-api/src/main/java/greencity/dto/user/FriendDto.java
@@ -0,0 +1,22 @@
+package greencity.dto.user;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode
+public class FriendDto {
+
+    private Long id;
+    private String profilePicture;
+    private String name;
+    private Double rating;
+    private String userCredo;
+    private String city;
+    private Integer mutualFriends;
+    private boolean isOnline;
+}

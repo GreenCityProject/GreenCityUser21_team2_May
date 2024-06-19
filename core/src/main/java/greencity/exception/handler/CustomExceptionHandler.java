4f26fd0eb2f4b803f42853d23bbf0e8049e769b7
RockTF <egorov2001egorr@gmail.com>
2024-06-19 13:02:46 +0300

    Remove unnecessary blank lines in code
    Removed extra blank lines in both `CustomExceptionHandler.java` and `NewsSubscriberController.java` to improve code readability and maintain consistent formatting throughout the codebase.

--- core/src/main/java/greencity/exception/handler/CustomExceptionHandler.java
+++ core/src/main/java/greencity/exception/handler/CustomExceptionHandler.java
@@ -384,7 +384,6 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     public final ResponseEntity<Object> handlePasswordSameAsOldException(
         PasswordSameAsOldException exception,
         WebRequest request) {
-
         log.info(exception.getMessage());
         ExceptionResponse exceptionResponse = new ExceptionResponse(getErrorAttributes(request));
         exceptionResponse.setMessage(exception.getMessage());
